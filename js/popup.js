5a72d7b646bcbed39dc3de91ac06fe774ac35e49
clean_water <shmizuyuta213@icloud.com>
2022-06-13 15:08:50 +0900

    init

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+let button1 = document.getElementById("button1")
+let button2 = document.getElementById("button2")
+
+
+button1.onclick = () =>{
+  alert('hello world');
+  button2.style.backgroundColor = 'red';
+}
