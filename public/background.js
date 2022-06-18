4a292e52d29826aeebf9992135e6c396219f3379
i-nakamura823 <i-nakamura@ist.osaka-u.ac.jp>
2022-06-18 13:42:50 +0900

    コピー機能いったんコミット

---
+++ public/background.js
@@ -0,0 +1,56 @@
+let selectionText;
+let history = [];
+let historyCounter = 0;
+
+const user = {
+    username: 'demo-user'
+};
+
+const copyText = (text) => {
+    history.push(text);
+    console.log("copy: "+text);
+}
+
+const clearList = (text) => {
+    history = [];
+    console.log("clear history!!");
+}
+
+const showList = (text) => {
+    console.log("<コピー履歴>");
+    history.forEach(element => console.log(element));
+}
+
+chrome.commands.onCommand.addListener((command) => {
+    console.log("-------------------------");
+    switch (command) {
+        case "createPost":
+            copyText(selectionText);
+            break;
+        case "deletePosts":
+            clearList(selectionText);
+            break;
+        case "showList":
+            showList(selectionText);
+            break;
+    }
+    //console.log(`Command "${command}" called selectionText:${selectionText}`);
+});
+
+chrome.runtime.onMessage.addListener(
+    function onMessageFunc(message, sender, sendResponse) {
+        if (message === 'get-user-data') {
+            sendResponse(user);
+        }
+        selectionText = message.message;
+        chrome.tabs.query({active: true}).then(tabs => {
+            const tab = tabs[0];
+            //console.log(`selection text[${message.message}] update by sender:${sender.tab.id}, active.tab.id:${tab.id}`);
+    
+            if (sender.tab.id === tab.id) {
+                selectionText = message.message;
+            }
+        })
+        return true;
+    }
+);
---
+++ public/content.js
@@ -0,0 +1,14 @@
+document.addEventListener('selectionchange', function (event) {
+    let selectionText = window.getSelection().toString();
+    console.log(`selection changed:${selectionText}`);
+
+    if (selectionText.length){
+        try{
+            chrome.runtime.sendMessage({
+                message: selectionText
+            });
+        }catch(error){
+            console.log(error);
+        }
+    }
+})
