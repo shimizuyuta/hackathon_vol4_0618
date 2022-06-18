01b38f5813672735da33df67121cd4fccd503ee2
DESKTOP-I5BEVNJ\jin_h <jin.codegym1217@gmail.com>
2022-06-18 18:29:06 +0900

    app.jsの変更、muiのインストール

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Txt from './components/Txt/Txt';
+import Top from './components/top/Top'
 
 function App() {
   return (
-  <div class="container" id="main">
-    <div class="content">
-      <ul id="list"></ul>
+    <div>
+      <Top />
     </div>
-    <textarea name="" 
-      type="text"
-      class="textArea" 
-      id="text"  
-      cols="30" 
-      rows="10" 
-      autofocus 
-      required
-    >
-    </textarea>
-    <div class="buttonArea">
-      <button id="button1">テンプレート1</button>
-      <button id="button2">テンプレート2</button>
-      <button id="button3">テンプレート3</button>
-    </div>
-    <Txt></Txt>
-  </div>
   );
 }
 
