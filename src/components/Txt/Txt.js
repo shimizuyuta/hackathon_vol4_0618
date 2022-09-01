278c14394149da17f838e168dadb8ca8a08a6c2d
clean_water <shmizuyuta213@icloud.com>
2022-09-02 01:16:50 +0900

    忘れてた

--- src/components/Txt/Txt.js
+++ src/components/Txt/Txt.js
@@ -1,25 +1,17 @@
-import React from "react"
-
-const download = (data, title="clip-roach.txt") => {
-  const newBlob = new Blob(data);
-  const objUrl = window.URL.createObjectURL(newBlob);
-  const link = document.createElement("a");
-  link.href = objUrl;
-  link.download = "clip-roach.txt";
-  link.click();
+const download = (data, title = 'clip-roach.txt') => {
+  const newBlob = new Blob(data)
+  const objUrl = window.URL.createObjectURL(newBlob)
+  const link = document.createElement('a')
+  link.href = objUrl
+  link.download = 'clip-roach.txt'
+  link.click()
   setTimeout(() => {
-    window.URL.revokeObjectURL(objUrl);
-  }, 250);
+    window.URL.revokeObjectURL(objUrl)
+  }, 250)
 }
 
 const Txt = (props) => {
-  return (
-
-    <button onClick={() => download(props.data)}>
-      Txtでエクスポート
-    </button>
-
-  )
+  return <button onClick={() => download(props.data)}>Txtでエクスポート</button>
 }
 
 //export {Txt, ExportTextCollector, ExportButton, isSelected}
