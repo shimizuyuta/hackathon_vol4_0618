63b201ac6c23a94607952b002ffe778baec429fa
clean_water <shmizuyuta213@icloud.com>
2022-09-13 03:06:24 +0900

    インデント変更

--- public/content.js
+++ public/content.js
@@ -1,16 +1,17 @@
 /*global chrome*/
-document.addEventListener('selectionchange', function () {
-    let selectionText = window.getSelection().toString();
-    if (selectionText.length){
-        try{
-            let payload = {
-                message: selectionText ,
-                type: 'message',
-            }
-            chrome.runtime.sendMessage(payload)
-        }
-        catch(error){
-            console.log(error,'エラー発生');
-        }
+document.addEventListener('selectionchange', function (value) {
+  console.log('value',value)
+  let selectionText = window.getSelection().toString()
+  console.log(selectionText)
+  if (selectionText.length) {
+    try {
+      let payload = {
+        message: selectionText,
+        type: 'message',
+      }
+      chrome.runtime.sendMessage(payload)
+    } catch (error) {
+      console.log(error, 'エラー発生')
     }
+  }
 })
