58ae41a2d0a44d41719981245e9dd615f6a52db8
i-nakamura823 <i-nakamura@ist.osaka-u.ac.jp>
2022-09-16 15:35:50 +0900

    オブジェクトを追加＋サイトのコードのテスト

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -12,7 +12,6 @@ import { deleteContent } from '../modules/chrome'
 import Tooltip from '@mui/material/Tooltip';
 import { useState, useEffect } from 'react'
 import Checkbox from '@mui/material/Checkbox';
-// import { valueToPercent } from '@mui/base'
 
 function History({datas,textData}) {
 
@@ -21,6 +20,61 @@ function History({datas,textData}) {
   const [isChecked, setIsChecked] = useState([])
   // チェックされているindexのリスト
   const [checkedIndex, setCheckedIndex] = useState([])
+  // オブジェクト
+  const [ob, setOb] = useState([])
+
+
+
+  // https://qiita.com/seira/items/ccdb70b63cdce097aeb3の内容
+  const [checkedItems, setCheckedItems] = useState({})
+  const [isBtnHide, setIsBtnHide] = useState(true)
+  const MyCheckBox = ({id, value, checked, onChange}) => {
+    return (
+      <input
+        id={id}
+        type="checkbox"
+        name="inputNames"
+        checked={checked}
+        onChange={onChange}
+        value={value}
+      />
+    )
+  }
+  useEffect(() => {
+    //checkedItemsが空では無い場合、送信ボタンを表示させる
+        Object.keys(checkedItems).length && setIsBtnHide(false)
+    //すべてのcheckedItemの値がfalseの場合に送信ボタンを表示させる
+        setTimeout(() => {
+          if (
+            Object.values(checkedItems).every(checkedItem => {
+              return checkedItem === false
+            })
+          ) {
+            setIsBtnHide(true)
+          }
+        },100);
+      }, [checkedItems])
+  const handleChange2 = e => {
+    setCheckedItems({
+      ...checkedItems,
+      [e.target.id]: e.target.checked
+    })
+    console.log('checkedItems:', checkedItems)
+  }
+  const dataSendBtn = e => {
+    //既定のイベントをキャンセルさせる
+        e.preventDefault()
+    //送信ボタンを押したタイミングで、checkedItemsオブジェクトのvalueがtrueのkeyのみを配列にしてconsoleに表示させる
+        const dataPushArray = Object.entries(checkedItems).reduce((pre,[key, value])=>{
+          value && pre.push(key)
+          return pre
+        },[])
+        console.log("dataPushArray:", dataPushArray)
+      }
+
+
+
+
 
   useEffect(() => {
     setText(textData)
@@ -35,6 +89,31 @@ function History({datas,textData}) {
       setIsChecked(datas.map((data, index) =>{
         return checkedIndex.includes(index)
       }))
+
+      // object版の処理
+      // datasが更新されたらtrueをセット
+      // 追加されたら以前の引継ぎ
+      // 削除されたらその要素だけ削除
+      // oldCheckを入れたらいい？？
+      // if(oldChecked.length < datas.length)(追加されたとき)
+      console.log('処理前object : ',ob)
+      if(!datas.length){
+        // setOb([])
+        console.log('全削除')
+      }else if(datas.length > ob.length){
+        console.log('追加')
+        let newIndex = datas.length-1
+        let newData = datas[newIndex]
+        // console.log(newData)
+        // setOb([...ob,{text:{newData},isChecked:true,index:{newIndex}}])
+        //ob.push({text:{datas[datas.length-1]}})
+      }else{
+        console.log('削除')
+      }
+      setOb(datas.map((data, index) =>{
+        return {text:{data},isChecked: true, index:{index}}
+      }))
+      //console.log('処理後object : ',ob)
     }
   },[datas])
 
@@ -58,10 +137,10 @@ function History({datas,textData}) {
   }
 
   const deleteAction = (index) => {
-    console.log('index : ',index)
-    console.log('checked : ',checkedIndex)
+    // console.log('index : ',index)
+    // console.log('checked : ',checkedIndex)
     setCheckedIndex(checkedIndex.filter(i => i !== index))
-    console.log('削除後 : ',checkedIndex)
+    // console.log('削除後 : ',checkedIndex)
     setCheckedIndex(checkedIndex.map(i => {
       if(i > index){
         return i - 1
@@ -74,7 +153,8 @@ function History({datas,textData}) {
 
   const handleClick = () => {
     checkedIndex.sort((first, second) => first - second);
-    console.log('検証 : ',checkedIndex)
+    //console.log('検証 : ',checkedIndex)
+    console.log('処理後object : ',ob)
   }
 
   const handleClick2 = () => {
@@ -128,6 +208,25 @@ function History({datas,textData}) {
       </Box>
       <button onClick={handleClick}>ちぇっく</button>
       <button onClick={handleClick2}>配列</button>
+      <h2>好きな食べ物</h2>
+      <form>
+        {datas && datas.map((item, index) => {
+          index = index + 1
+          return (
+            <label htmlFor={`id_${index}`} key={`key_${index}`}>
+              <MyCheckBox
+                id={`id_${index}`}
+                value={item}
+                onChange={handleChange2}
+                checked={checkedItems[item.id]}
+              />
+              {item}
+            </label>
+          )
+        })}
+{/* checkedがない場合には送信ボタンを表示させない */}
+        {!isBtnHide && <button onClick={dataSendBtn}>アンケート送信ボタン</button>}
+      </form>
       <Box
         sx={{
           minHeight: '30vmx',
