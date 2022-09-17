45c94ad91f59c25b84c453678a401cd875de892b
clean_water <shmizuyuta213@icloud.com>
2022-09-18 00:41:59 +0900

    チェックボックス（一つしか削除できない）

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -5,23 +5,79 @@ import ListItemText from '@mui/material/ListItemText'
 import IconButton from '@mui/material/IconButton'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import DeleteIcon from '@mui/icons-material/Delete'
-import TextField from '@mui/material/TextField'
-import { Box } from '@mui/system'
-import InputAdornment from '@mui/material/InputAdornment'
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
+// import TextField from '@mui/material/TextField'
+// import InputAdornment from '@mui/material/InputAdornment'
 import { CopyToClickboard } from '../modules/index'
 import { deleteContent } from '../modules/chrome'
-import Tooltip from '@mui/material/Tooltip';
 import { useState, useEffect, createRef } from 'react'
+import Tooltip from '@mui/material/Tooltip'
+import { Box } from '@mui/system'
+import Checkbox from '@mui/material/Checkbox'
 
-function History({datas,textData}) {
-
+function History({ datas, textData }) {
   const [text, setText] = useState('text')
   const ref = createRef()
+  const [checkedItems, setCheckedItems] = useState([])
+  console.log(datas, 'datas')
 
+  //[{item: "'AA',", checked: false},{item: "'BB',", checked: false}]
+  const handleChange = (e) => {
+    let zzz = new Array()
+    checkedItems.map((checkedItem, index) => {
+      console.log(checkedItem, 'checkedItemだよ')
+      //☑が入っているとき→[{item: "'AA',", checked: true},{item: "'BB',", checked: false}]
+      if (checkedItem['item']===e.target.value && checkedItem['checked']===true) {
+        checkedItem['checked'] = false
+        zzz.push(checkedItem)
+      }else if(checkedItem['item']===e.target.value){
+        checkedItem['checked'] = true
+        zzz.push(checkedItem)
+      }else {
+        //入ってないとき
+        zzz.push(checkedItem)
+        console.log('該当しないからそのまま配列に入る')
+      }
+      setCheckedItems(zzz)
+    })
+  }
+  console.log('checkedItems調べ',checkedItems)
+
+  const deleteCheckedItem = (index,checkedItem) =>{
+    console.log(index,'index')
+    console.log(checkedItem,'checkedItem')
+    //ストレージから削除
+    deleteContent(index)
+    //   //[{item: "'AA',", checked: false},{item: "'BB',", checked: false}]
+    console.log(checkedItems,'checkedItems++')
+      checkedItems.splice(index,1)
+    console.log('更新後のcheckedItems',checkedItems)
+    setCheckedItems(checkedItems)
+  }
+
+  const bbb = () => {
+    console.log('fafa', checkedItems)
+  }
   useEffect(() => {
     setText(textData)
-  },[textData])
+  }, [textData])
+
+  useEffect(() => {
+    console.log('datas === undefined', datas === undefined)
+    if (datas === undefined) {
+      setCheckedItems([])
+    } else {
+      console.log('ここ？',datas)
+      let sample = datas.map((data) => {
+        return { item: data, checked: false }
+      })
+      console.log('これが追加されるお', [...sample])
+      setCheckedItems([ ...sample])
+    }
+  }, [datas])
+  //[]これは最初に呼ばれてるからダメ
+  //[datas] useeffect/uselayout両方ダメ
+  //毎回：ダメ
+  //
 
   return (
     <>
@@ -33,7 +89,8 @@ function History({datas,textData}) {
         }}
       >
         {datas &&
-          datas.map((value, index) => (
+          checkedItems.length !== 0 &&
+          datas.map((data, index) => (
             <List
               sx={{
                 padding: '5px',
@@ -47,25 +104,34 @@ function History({datas,textData}) {
               <ListItem key={index} disablePadding sx={{ paddingTop: '5px' }}>
                 <Box sx={{ width: '30px' }}>
                   <ListItemIcon>
-                    <KeyboardArrowRightIcon />
+                    <Checkbox
+                      // checked={true}
+                      checked={checkedItems[index]['checked']}
+                      onChange={handleChange}
+                      id={index}
+                      value={data}
+                    />
                   </ListItemIcon>
                 </Box>
-                <ListItemText className="ellipsis" id={index} primary={value} />
-                  <Tooltip title="コピー">
-                    <IconButton onClick={() => CopyToClickboard(value)}>
-                      <ContentCopyIcon />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="削除">
-                    <IconButton
-                      onClick={() => {
-                        deleteContent(index)
-                      }}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </Tooltip>
+                <button onClick={bbb}>fafafa</button>
+                <ListItemText className='ellipsis' id={index} primary={data} />
+                <Tooltip title='コピー'>
+                  <IconButton onClick={() => CopyToClickboard(data)}>
+                    <ContentCopyIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title='削除'>
+                  <IconButton
+                    onClick={() => {
+                      // deleteContent(index)
+                      deleteCheckedItem(index,checkedItems[index])
+                    }}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Tooltip>
               </ListItem>
+              {/* <button onClick={ a }>a click</button> */}
             </List>
           ))}
       </Box>
@@ -76,7 +142,7 @@ function History({datas,textData}) {
           width: '100%',
         }}
       >
-        <TextField
+        {/* <TextField
           id='outlined-textarea'
           // value={textData}
           inputRef={ref}
@@ -96,7 +162,7 @@ function History({datas,textData}) {
             ),
           }}
           sx={{ width: '470px' }}
-        />
+        /> */}
       </Box>
     </>
   )
